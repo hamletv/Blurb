527b2949bc895e8bf29ece040bede9361c0f2ba0
Hamlet <hamlet.villa@gmail.com>
2019-12-09 11:46:40 -0500

    sample routes and base template added, index template optimized

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Hamlet'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'It is a very rainy day in New York City today.'
+        },
+        {
+            'author': {'username': 'Peter'},
+            'body': 'How about the news about the impeachment?!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
