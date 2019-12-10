51b8de11d97a8e7cb50850577d4c599675e235f7
Hamlet <hamlet.villa@gmail.com>
2019-12-09 23:33:08 -0500

    database: migration, models setup; adjust config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy # import, create instances of database and migration engine
+from flask_migrate import Migrate
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+
+from app import routes, models  # models to define structure of db
