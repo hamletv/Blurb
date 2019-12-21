e7cac6c8b396f2dfc0e64d7310464fe4eabd5c4f
Hamlet <hamlet.villa@gmail.com>
2019-12-21 08:07:04 -0800

    User class added follow, unfollow and supporting methods

--- app/models.py
+++ app/models.py
@@ -43,6 +43,17 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
