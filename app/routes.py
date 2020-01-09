87aaefb3dfc8e075baa115d3cc606c16ba407441
Hamlet <hamlet.villa@gmail.com>
2020-01-09 14:01:10 -0500

    post form added to view method

--- app/routes.py
+++ app/routes.py
@@ -9,13 +9,21 @@ from flask_login import logout_user
 from flask_login import login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
-from app.forms import EditProfileForm
+from app.forms import EditProfileForm, PostForm
+from app.models import Post
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'John'},
