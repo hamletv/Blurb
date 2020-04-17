f54a7d7e47b7ffb971a874d597bfbffa7c04ec31
Hamlet <hamlet.villa@gmail.com>
2020-04-17 14:49:34 -0400

    assign id numbers to posts and translations in post template

--- app/__init__.py
+++ app/__init__.py
@@ -41,19 +41,19 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Error!',
+            toaddrs=app.config['ADMINS'], subject='Blurb Error!',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler('logs/blurb.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineo)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('Blurb startup')
 
 from app import routes, models, errors  # imported modules from app package/repo. models to define structure of db
