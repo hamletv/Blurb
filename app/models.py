dba6c8fe88b3617a42e2e3f9c1925c8cd5e946ec
Hamlet <hamlet.villa@gmail.com>
2020-05-04 07:56:04 -0400

    update to current_app

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import app, db, login
+from app import db, login
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
 import jwt
+from flask import current_app
 
 @login.user_loader
 def load_user(id):
@@ -64,12 +65,12 @@ class User(UserMixin, db.Model):
 
     def get_reset_password_token(self, expires_in=600):         # generate JWT token as string for convenience
         return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf8')
 
     @staticmethod
     def verify_reset_password_token(token):         # static method, doesn't receive class (self) as first argument
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
