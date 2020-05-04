0afd86c4a280a86b995c4cc26eab81ee847e6a56
Hamlet <hamlet.villa@gmail.com>
2020-05-04 07:31:16 -0400

    auth and main forms modules fixed

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -31,27 +31,6 @@ class RegistrationForm(FlaskForm):          # user registration form
             raise ValidationError(_('Please use a different email address.'))
 
 
-class EditProfileForm(FlaskForm):
-    username = StringField(_lg('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_lg('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_lg('Submit'))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError(_('Please use a different username.'))
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_lg('Say something'), validators=[DataRequired(), Length(min=1, max=140)])
-    submit= SubmitField(_lg('Submit'))
-
-
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField(_lg('Email'), validators=[DataRequired(), Email()])
     submit = SubmitField(_lg('Request Password Reset'))
